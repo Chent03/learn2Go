3fab09e56ed740c398314945a5b699e4bb871e79
chent03 <tc56007n@pace.edu>
2019-10-03 23:40:19 -0400

    2.3 Dyname reloading

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site</h1>")
+	fmt.Println("Someone visited my page")
+	fmt.Fprintf(w, "<h1>Welcome to my super awesome site</h1>")
 }
 
 func main() {
